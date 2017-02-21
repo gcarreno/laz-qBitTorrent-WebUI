d3bc8706563d897f867e3de218cded2d8f1460e0
Gustavo Carreno <guscarreno@gmail.com>
2017-02-21 20:03:09 +0000

    TqBTorrentState and TqBTorrent added.

--- src/qBTorrents.pas
+++ src/qBTorrents.pas
@@ -31,43 +31,107 @@ uses
   Classes, Contnrs, SysUtils;
 
 type
-  { TqBTorrent }
-    TqBTorrent = class(TObject)
-    private
-      FHash: String;
-      FName: String;
-      FSize: Int64;
-      FProgress: Double;
-      FDlSpeed: Integer;
-      FUpSpeed: Integer;
-      FPriority: Integer;
-    protected
-    public
-      constructor Create;
-      destructor Destroy; override;
+{ TqBTorrentStates }
+  TqBTorrentState = (
+    tsError,
+    tsPausedUp,
+    tsPausedDl,
+    tsQueuedUp,
+    tsQueuedDl,
+    tsUploading,
+    tsStalledUp,
+    tsStalledDl,
+    tsCheckingUp,
+    tsCheckingDl,
+    tsDownloading,
+    tsMetaDl,
+    tsUnknown
+  );
+{ TqBTorrent }
+  TqBTorrent = class(TObject)
+  private
+    FHash: String;
+    FName: String;
+    FSize: Int64;
+    FProgress: Double;
+    FDlSpeed: Integer;
+    FUpSpeed: Integer;
+    FPriority: Integer;
+    FNumSeeds: Integer;
+    FNumComplete: Integer;
+    FNumLeechs: Integer;
+    FNumIncomplete: Integer;
+    FRatio: Double;
+    FEta: Integer; //Seconds
+    FState: TqBTorrentState;
+    FSeqDl: Boolean;
+    FFirstLastPiecePrioritized: Boolean;
+    FCategory: String;
+    FSuperSeeding: Boolean;
+    FForceStart: Boolean;
+  protected
+  public
+    constructor Create;
+    destructor Destroy; override;
 
-      property Hash: String
-        read FHash
-        write FHash;
-      property Name: String
-        read FName
-        write FName;
-      property Size: Int64
-        read FSize
-        write FSize;
-      property Progress: Double
-        read FProgress
-        write FProgress;
-      property DlSpeed: Integer
-        read FDlSpeed
-        write FDlSpeed;
-      property UpSpeed: Integer
-        read FUpSpeed
-        write FUpSpeed;
-      property Priority: Integer
-        read FPriority
-        write FPriority;
-    end;
+    property Hash: String
+      read FHash
+      write FHash;
+    property Name: String
+      read FName
+      write FName;
+    property Size: Int64
+      read FSize
+      write FSize;
+    property Progress: Double
+      read FProgress
+      write FProgress;
+    property DlSpeed: Integer
+      read FDlSpeed
+      write FDlSpeed;
+    property UpSpeed: Integer
+      read FUpSpeed
+      write FUpSpeed;
+    property Priority: Integer
+      read FPriority
+      write FPriority;
+    property NumSeeds: Integer
+      read FNumSeeds
+      write FNumSeeds;
+    property NumComplete: Integer
+      read FNumComplete
+      write FNumComplete;
+    property NumLeechs: Integer
+      read FNumLeechs
+      write FNumLeechs;
+    property NumIncomplete: Integer
+      read FNumIncomplete
+      write FNumIncomplete;
+    property Ratio: Double
+      read FRatio
+      write FRatio;
+    property Eta: Integer // Seconds
+      read FEta
+      write FEta;
+    property State: TqBTorrentState
+      read FState
+      write FState;
+    property SeqDl: Boolean
+      read FSeqDl
+      write FSeqDl;
+    property FirstLastPiecePrioritized: Boolean
+      read FFirstLastPiecePrioritized
+      write FFirstLastPiecePrioritized;
+    property Category: String
+      read FCategory
+      write FCategory;
+    property SuperSeeding: Boolean
+      read FSuperSeeding
+      write FSuperSeeding;
+    property ForceStart: Boolean
+      read FForceStart
+      write FForceStart;
+  end;
 
 { TqBTorrents }
   TqBTorrents = class(TFPObjectList)
@@ -81,7 +145,95 @@ type
       write SetItem; default;
   end;
 
+// Helper functions
+function StrToqBState(const aState: String): TqBTorrentState;
+function qBStateToStr(aState: TqBTorrentState): String;
+
 implementation
+
+function StrToqBState(const aState: String): TqBTorrentState;
+begin
+  Result := tsUnknown;
+  if aState = 'error' then
+  begin
+    Result := tsError;
+    exit;
+  end;
+  if aState = 'pausedUP' then
+  begin
+    Result := tsPausedUp;
+    exit;
+  end;
+  if aState = 'pausedDL' then
+  begin
+    Result := tsPausedDl;
+    exit;
+  end;
+  if aState = 'queuedUP' then
+  begin
+    Result := tsQueuedUp;
+    exit;
+  end;
+  if aState = 'queuedDL' then
+  begin
+    Result := tsQueuedDl;
+    exit;
+  end;
+  if aState = 'uploading' then
+  begin
+    Result := tsUploading;
+    exit;
+  end;
+  if aState = 'stalledUP' then
+  begin
+    Result := tsStalledUp;
+    exit;
+  end;
+  if aState = 'stalledDL' then
+  begin
+    Result := tsStalledDl;
+    exit;
+  end;
+  if aState = 'checkingUP' then
+  begin
+    Result := tsCheckingUp;
+    exit;
+  end;
+  if aState = 'checkingDL' then
+  begin
+    Result := tsCheckingDl;
+    exit;
+  end;
+  if aState = 'downloading' then
+  begin
+    Result := tsDownloading;
+    exit;
+  end;
+  if aState = 'metaDL' then
+  begin
+    Result := tsMetaDl;
+    exit;
+  end;
+end;
+
+function qBStateToStr(aState: TqBTorrentState): String;
+begin
+  Result := 'unknown';
+  case aState of
+    tsError:       Result := 'error';
+    tsPausedUp:    Result := 'pausedUP';
+    tsPausedDl:    Result := 'pausedDL';
+    tsQueuedUp:    Result := 'queuedUP';
+    tsQueuedDl:    Result := 'queuedDL';
+    tsUploading:   Result := 'uploading';
+    tsStalledUp:   Result := 'stalledUP';
+    tsStalledDl:   Result := 'stalledDL';
+    tsCheckingUp:  Result := 'checkingUP';
+    tsCheckingDl:  Result := 'checkingDL';
+    tsDownloading: Result := 'downloading';
+    tsMetaDl:      Result := 'metaDL';
+  end;
+end;
 
 { TqBTorrent }
 
